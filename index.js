d6d255ed418ff8072adf84e124b42fc59e437d46
felipe-fma <felipe.moura.arantes@hotmail.com>
2019-08-28 00:53:01 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,22 @@
+const restify = require('restify')
+const porta = 8080
+
+const servidor = restify.createServer()
+
+servidor.get('/', (req, res) => {
+	res.send('Ae! Servidor criado veio com resposta para o path /.')
+	
+})
+
+servidor.post('/', (req, res) => {
+  res.send('Ae! Servidor criado veio com resposta para o path /.')
+})
+
+servidor.del('/', (req, res) => {
+  res.send('Ae! Servidor criado veio com resposta para o path /.')
+})
+
+servidor.listen(porta, () => {
+  console.log(`Servidor de pé em http://localhost:${porta}`)
+  console.log('Pra derrubar o servidor: ctrl + c')
+})
