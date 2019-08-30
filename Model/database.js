640c85d65c224abf327b8952577f444c153db062
felipe-fma <54600054+felipe-fma@users.noreply.github.com>
2019-08-30 15:56:53 -0300

    Subindo final server gypz

---
+++ Model/database.js
@@ -0,0 +1,30 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(':memory:');
+
+module.exports =
+{  
+    createTable()
+    {
+        db.run(`CREATE TABLE CARTOES (id INTEGER PRIMARY KEY, nome TEXT, limite NUMERIC(10,2), renda NUMERIC(10,2))`, (err, row) => { console.log(row);  console.log(err) });
+    },
+
+    insert(nome, limite, renda, call)
+    {
+        db.run(`INSERT INTO CARTOES(nome, limite, renda) VALUES ('${nome}', ${limite}, ${renda})`, (err, row) => { call(err, row) });
+    },
+
+    getAll(call)
+    {
+        db.all(`SELECT * FROM CARTOES `, (err, row) => call(row) ); 
+    },
+
+    get(id, call)
+    {
+        db.all(`SELECT * FROM CARTOES WHERE id = ${id}`, (err, row) => call(err, row) ); 
+    },
+
+    del(id, call)
+    {
+        db.run(`DELETE FROM CARTOES WHERE id = ${id}`, (err, row) => call(err, row));
+    }
+};
--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 const restify = require('restify')
-const porta = 8080
+var database = require('./Model/database')
+var porta = 8081
 
-const servidor = restify.createServer()
+var servidor = restify.createServer({
+    name: 'Hello World!',
+    version: '1.0.0'
+});
 
-servidor.get('/', (req, res) => {
-	res.send('Ae! Servidor criado veio com resposta para o path /.')
-	
+servidor.use(restify.plugins.queryParser({ mapParams: true }));
+servidor.use(restify.plugins.bodyParser({ mapParams: true }));
+servidor.use(restify.plugins.acceptParser(servidor.acceptable));
+
+servidor.get('/', (req, res) => 
+{
+  var s = function(row){
+    res.send(row);
+  }
+  database.getAll(s);
 })
 
-servidor.post('/', (req, res) => {
-  res.send('Ae! Servidor criado veio com resposta para o path /.')
+servidor.post('/', (req, res) => 
+{
+  const { nome, renda } = req.body;  
+  var score = parseInt(Math.random() * 1000);
+
+  var lErr = true;
+  var limite = 0;
+
+  if (score <= 299)
+    lErr = false;
+  
+  if (score >= 300 && score <= 599)
+    limite = 1000;
+
+  if (score >= 600 && score <= 799)
+    limite = renda * 1.5;
+
+  if (score >= 800 && score <= 950)
+    limite = renda * 2;
+
+  if (score >= 951 )
+    limite = 0;
+
+  if(lErr)
+    database.insert(nome, limite, renda, (erro, row) => {
+      if(erro == undefined)
+        res.send(`Limite concedido de : ${limite}`);
+      else 
+        res.send(`Deu erro ${erro}`)
+    });
+  else
+    res.send(`Score baixo ${score}`);
 })
 
-servidor.del('/', (req, res) => {
-  res.send('Ae! Servidor criado veio com resposta para o path /.')
+servidor.del('/', (req, res) => 
+{
+  const { id } = req.body;  
+
+  database.del(id, (err, row) => {
+    if(err == null)
+      res.send(`Cliente deletado`);
+    else
+      res.send(`Erro ${err}`);
+  });
 })
 
-servidor.listen(process.env.PORT || porta, () => {
-
+servidor.listen(process.env.PORT || porta, () => 
+{
+  database.createTable();
 })
